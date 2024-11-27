05655246d5f9e5fb401a1be3aec91c8c1a4bd1d8
Maxim-Osincev <osince@list.ru>
2024-11-27 10:55:04 +0300

    First

--- src/components/main/ItemsList.vue
+++ src/components/main/ItemsList.vue
@@ -12,10 +12,11 @@ export default {
 </script>
 
 <template>
-  <div>
+  <div class="items_list__wrapper">
     <div
       v-for="item in items"
       :key="item.id"
+      class="items_list__list-item"
       @click="$emit('item-click', item)"
     >
       {{ item.name }}
@@ -24,5 +25,17 @@ export default {
 </template>
 
 <style scoped>
-
+.items_list__wrapper {
+  padding: 8px;
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  border: 1px solid #000000;
+}
+.items_list__list-item {
+  padding: 8px;
+  border: 1px solid #000000;
+  width: 50px;
+  height: 50px;
+}
 </style>
--- src/views/MainView.vue
+++ src/views/MainView.vue
@@ -12,12 +12,38 @@ export default {
 </script>
 
 <template>
-  <div>
-    <user-items />
-    <available-items />
+  <div class="main__wrapper">
+    <user-items class="main__col" />
+    <available-items class="main__col" />
   </div>
 </template>
 
-<style scoped>
-
+<style>
+.main__wrapper {
+  display: flex;
+  flex-wrap: nowrap;
+  gap: 16px;
+  justify-content: space-between;
+}
+.main__col {
+  flex: 0 1 50%;
+}
+.items__selected {
+  border: 1px solid #000000;
+  padding: 8px;
+  margin-bottom: 16px;
+  min-height: 30px;
+}
+.items__selected_list {
+  display: flex;
+  flex-wrap: wrap;
+  gap: 8px;
+  margin-bottom: 8px;
+}
+.items__selected-item {
+  padding: 8px;
+  border: 1px solid #000000;
+  width: 50px;
+  height: 50px;
+}
 </style>
